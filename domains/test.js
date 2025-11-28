2baedc90f1795cc293c18aaf7a642ddc9d99379f
YusupKakuu <nessxyz29@gmail.com>
2025-11-29 00:57:44 +0700

    Create test.js

---
+++ domains/test.js
@@ -0,0 +1,17 @@
+{
+    "repo": "https://github.com/ItsukaShidouu/nss.biz.id",
+
+    "owner": {
+        "username": "ItsukaShidouu",
+        "email": "nessxyz29@gmail.com"
+    },
+
+    "target": {
+        "CNAME": {
+            "name": "test",
+            "value": "ItsukaShidouu.github.io"
+        }
+    },
+
+    "proxied": false
+}
